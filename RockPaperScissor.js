145f35a0c0eda113e4e2a6e0a7cf6c54175b42f9
Shawn Franklin <shawnbruin@gmail.com>
2022-09-26 21:30:13 +0200

    paper scissors functions. alert asks for each  entry and give unique answer for each round in  console. problem is anywhere on screen click  produces what only button should do. need to  confine click to only button.

--- RockPaperScissor.js
+++ RockPaperScissor.js
@@ -1,54 +1,56 @@
+const userInput = ["Rock", "Paper", "Scissors"];
+
 function getComputerChoice() {
     /randomly return variable Rock Paper Scissors/
-    let game = ["Rock", "Paper", "Scissors"];
-    const result = game[Math.floor(Math.random() * game.length)]; console.log(result);
+    const result = userInput[Math.floor(Math.random() * userInput.length)]; 
+    console.log(result);
     return result;
+    
 }
 /* 1. Player inputs selection
    2. computer makes selection
    3. we compare the playerSelection to computerSelection
    4. declare winner of game */
 
-function loadGame(playerSelection, computerSelection) {
-           
-            if (playerSelection === "Rock" && computerSelection === "Scissors") {
-            return "YOU WIN! ROCK BEATS SCISSORS";
+function playRound(userInput, computerInput) {
+    var playerSelection = prompt("Please choose Rock, Paper or Scissors!").toLowerCase();
+    var computerSelection = getComputerChoice(); 
+         userInput = playerSelection.toLowerCase();
+         computerInput = computerSelection.toLowerCase();
+        if(userInput == "rock" && computerInput == "scissors") {
+            return "You Win!"; }
+        else if(userInput == "scissors" && computerSelection == "paper") {
+            return "You Win!";
         }
-            else if (playerSelection === "Rock" && computerSelection === "Paper") {
-            return "YOU LOSE, PAPER BEATS ROCK!";
+        else if(userInput == "paper" && computerInput == "rock") {
+            return "You Win!"
         }
-
-            else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-        return "YOU LOSE! ROCK BEATS SCISSORS";
+        else if(userInput == computerInput) {
+            return "It's a Draw!"
         }
-
-            else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-        return "YOU WIN, SCISSORS CUTS PAPER!";
-        }
-
-            else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-            return "YOU LOSE! SCISSORS BEATS PAPER";
-        }
-            else if (playerSelection === "Paper" && computerSelection === "Rock") {
-            return "YOU WIN, PAPER BEATS ROCK!";
-        }
-
-            else if (playerSelection === "Rock" && computerSelection === "Rock" || 
-                playerSelection === "Paper" && computerSelection === "Paper" ||
-                playerSelection === "Scissors" && computerSelection === "Scissors")  {
-            return "IT'S A DRAW!"; 
-        }
-
-            else {
-            return "Invalid Selection!";
+        else {
+            return "You Lose!"
         }
 }   
 
-const playerSelection = prompt("Please choose Rock, Paper or Scissors!");
-const computerSelection = getComputerChoice();
 
+function game(e) {
+   /* 1. Run game five times and get a different result without restarting page 
+      2.  */
+      e.preventDefault(); 
+      for(var times = 1; times < 6; times++) {
+            console.log(times);
+            var result = console.log(playRound(e));
+            result;   
+        };
+    }
+var result = document.getElementById('result');
+var gameTime = document.addEventListener('click', game);
 
+//when click button game should run producing a result to be displayed under game time 
 
-console.log(loadGame(playerSelection, computerSelection));
+//function runGame(e) {
+   // var result = document.getElementById('result');
+ //  var gameResult = result.innerHTML = game(e);
 
 
